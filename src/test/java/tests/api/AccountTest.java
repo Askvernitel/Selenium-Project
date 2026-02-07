8607714d52facfac2b78dd6d3605cf7f7c0a1983
Askvernitel <danielkolotashvilio@gmail.com>
2026-02-07 17:04:58 +0400

    feat: add allure logging to API test classes

--- src/test/java/tests/api/AccountTest.java
+++ src/test/java/tests/api/AccountTest.java
@@ -1,10 +1,8 @@
 package tests.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.qameta.allure.*;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import org.apache.hc.core5.http.HttpStatus;
 import org.project.base.ApiTestBase;
 import org.project.client.AccountClient;
@@ -13,9 +11,7 @@ import org.project.utils.ApiUtils;
 import org.project.utils.TestDataFactory;
 import org.testng.annotations.Test;
 
-
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.empty;
 
 @Epic("API Tests")
 @Feature("Account Management")
@@ -23,21 +19,17 @@ public class AccountTest extends ApiTestBase {
 
     private final AccountClient accountClient = new AccountClient();
 
-
     @Test
     @Story("Account Creation")
     @Description("Verify that a new account can be created via API")
     @Severity(SeverityLevel.BLOCKER)
-    public void createAccount(){
+    public void createAccount() {
         Account account = TestDataFactory.getAccount();
         ApiUtils.attachRequest(account);
         accountClient.deleteAccount(account);
+
         Response response = accountClient.createAccount(account);
-        response.then()
-                .statusCode(HttpStatus.SC_SUCCESS)
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(201))
-                .body("message", equalTo("User created!"));
+        response.then().statusCode(HttpStatus.SC_SUCCESS).parser("text/html", Parser.JSON).body("responseCode", equalTo(201)).body("message", equalTo("User created!"));
 
         ApiUtils.attachResponse(response);
     }
@@ -46,92 +38,87 @@ public class AccountTest extends ApiTestBase {
     @Story("Account Deletion")
     @Description("Verify that an account can be deleted via API")
     @Severity(SeverityLevel.CRITICAL)
-    public void deleteAccount(){
+    public void deleteAccount() {
         Account account = TestDataFactory.getAccount();
+        ApiUtils.attachRequest(account);
         accountClient.createAccount(account);
-        accountClient.deleteAccount(account)
-                .then()
-                .statusCode(HttpStatus.SC_SUCCESS)
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(200))
-                .body("message", equalTo("Account deleted!"));
 
-        accountClient.getAccountByEmail(account.getEmail())
-                .then()
-                .body("responseCode", not(equalTo(200)));
+        Response response = accountClient.deleteAccount(account);
+        response.then().statusCode(HttpStatus.SC_SUCCESS).parser("text/html", Parser.JSON).body("responseCode", equalTo(200)).body("message", equalTo("Account deleted!"));
+
+        ApiUtils.attachResponse(response);
+
+        Response verifyResponse = accountClient.getAccountByEmail(account.getEmail());
+        verifyResponse.then().body("responseCode", not(equalTo(200)));
+
+        ApiUtils.attachResponse(verifyResponse);
     }
-
 
     @Test
     @Story("Account Update")
     @Description("Verify that account information can be updated via API")
     @Severity(SeverityLevel.CRITICAL)
-    public void updateAccount(){
+    public void updateAccount() {
         Account account = TestDataFactory.getAccount();
+        ApiUtils.attachRequest(account);
         accountClient.createAccount(account);
 
         account.setName("Test");
-        accountClient.updateAccount(account)
-                        .then()
-                        .statusCode(HttpStatus.SC_SUCCESS)
-                        .parser("text/html", Parser.JSON)
-                        .body("responseCode", equalTo(200))
-                        .body("message", equalTo("User updated!"));
+        ApiUtils.attachRequest(account);
 
-        accountClient.getAccountByEmail(account.getEmail())
-                .then()
-                .parser("text/html", Parser.JSON)
-                .body("user.name", equalTo("Test"));
+        Response response = accountClient.updateAccount(account);
+        response.then().statusCode(HttpStatus.SC_SUCCESS).parser("text/html", Parser.JSON).body("responseCode", equalTo(200)).body("message", equalTo("User updated!"));
+
+        ApiUtils.attachResponse(response);
+
+        Response verifyResponse = accountClient.getAccountByEmail(account.getEmail());
+        verifyResponse.then().parser("text/html", Parser.JSON).body("user.name", equalTo("Test"));
+
+        ApiUtils.attachResponse(verifyResponse);
     }
-
 
     @Test
     @Story("Account Login")
     @Description("Verify that login succeeds with valid credentials via API")
     @Severity(SeverityLevel.BLOCKER)
-    public void loginValidCredentials(){
+    public void loginValidCredentials() {
         Account account = TestDataFactory.getAccount();
-
+        ApiUtils.attachRequest(account);
         accountClient.createAccount(account);
 
-        accountClient.postVerifyLogin(account.getEmail(), account.getPassword())
-                .then()
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(200))
-                .body("message", equalTo("User exists!"));
+        Response response = accountClient.postVerifyLogin(account.getEmail(), account.getPassword());
+        response.then().parser("text/html", Parser.JSON).body("responseCode", equalTo(200)).body("message", equalTo("User exists!"));
+
+        ApiUtils.attachResponse(response);
     }
 
     @Test
     @Story("Account Login")
     @Description("Verify that login fails with invalid credentials via API")
     @Severity(SeverityLevel.CRITICAL)
-    public void loginInvalidCredentials(){
+    public void loginInvalidCredentials() {
         Account account = TestDataFactory.getAccount();
-
+        ApiUtils.attachRequest(account);
         accountClient.deleteAccount(account);
 
-        accountClient.postVerifyLogin(account.getEmail(), account.getPassword())
-                .then()
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", not(equalTo(200)))
-                .body("message", not(equalTo("User exists!")));
+        Response response = accountClient.postVerifyLogin(account.getEmail(), account.getPassword());
+        response.then().parser("text/html", Parser.JSON).body("responseCode", not(equalTo(200))).body("message", not(equalTo("User exists!")));
+
+        ApiUtils.attachResponse(response);
     }
+
     @Test
     @Story("Account Login")
     @Description("Verify that login endpoint returns proper error for unsupported HTTP method")
     @Severity(SeverityLevel.NORMAL)
-    public void loginBadRequest(){
+    public void loginBadRequest() {
         Account account = TestDataFactory.getAccount();
-
+        ApiUtils.attachRequest(account);
         accountClient.createAccount(account);
-        accountClient.deleteVerifyLogin(account.getEmail(), account.getPassword())
-                .then()
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(HttpStatus.SC_METHOD_NOT_ALLOWED))
-                .body("message", equalTo("This request method is not supported."));
+
+        Response response = accountClient.deleteVerifyLogin(account.getEmail(), account.getPassword());
+        response.then().parser("text/html", Parser.JSON).body("responseCode", equalTo(HttpStatus.SC_METHOD_NOT_ALLOWED)).body("message", equalTo("This request method is not supported."));
+
+        ApiUtils.attachResponse(response);
     }
-
-
-
-
 }
--- src/test/java/tests/api/ProductTest.java
+++ src/test/java/tests/api/ProductTest.java
@@ -2,9 +2,11 @@ package tests.api;
 
 import io.qameta.allure.*;
 import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
 import org.apache.hc.core5.http.HttpStatus;
 import org.project.base.ApiTestBase;
 import org.project.client.ProductClient;
+import org.project.utils.ApiUtils;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.Matchers.*;
@@ -15,62 +17,47 @@ public class ProductTest extends ApiTestBase {
 
     private final ProductClient productClient = new ProductClient();
 
-
     @Test
     @Story("Get Products")
     @Description("Verify that all products can be retrieved via API")
     @Severity(SeverityLevel.CRITICAL)
-    public void getAllProducts(){
-        productClient
-                .getAllProducts()
-                .then()
-                .statusCode(HttpStatus.SC_SUCCESS)
-                .using()
-                    .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(200))
-                .body("products", is(not(empty())));
+    public void getAllProducts() {
+        Response response = productClient.getAllProducts();
+        response.then().statusCode(HttpStatus.SC_SUCCESS).using().parser("text/html", Parser.JSON).body("responseCode", equalTo(200)).body("products", is(not(empty())));
 
+        ApiUtils.attachResponse(response);
     }
-
 
     @Test
     @Story("Get Brands")
     @Description("Verify that all brands can be retrieved via API")
     @Severity(SeverityLevel.NORMAL)
-    public void getAllBrands(){
-        productClient
-                .getAllBrands()
-                .then()
-                .statusCode(HttpStatus.SC_SUCCESS)
-                .using()
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(200))
-                .body("brands", is(not(empty())));
+    public void getAllBrands() {
+        Response response = productClient.getAllBrands();
+        response.then().statusCode(HttpStatus.SC_SUCCESS).using().parser("text/html", Parser.JSON).body("responseCode", equalTo(200)).body("brands", is(not(empty())));
+
+        ApiUtils.attachResponse(response);
     }
 
     @Test
     @Story("Get Products")
     @Description("Verify that POST method is not allowed for getting all products")
     @Severity(SeverityLevel.NORMAL)
-    public void postAllProducts(){
-        productClient
-                .postAllProducts()
-                .then()
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(405));
+    public void postAllProducts() {
+        Response response = productClient.postAllProducts();
+        response.then().parser("text/html", Parser.JSON).body("responseCode", equalTo(405));
+
+        ApiUtils.attachResponse(response);
     }
 
     @Test
     @Story("Get Brands")
     @Description("Verify that PUT method is not allowed for getting all brands")
     @Severity(SeverityLevel.NORMAL)
-    public void postAllBrands(){
-        productClient
-                .putAllBrands()
-                .then()
-                .parser("text/html", Parser.JSON)
-                .body("responseCode", equalTo(405));
+    public void postAllBrands() {
+        Response response = productClient.putAllBrands();
+        response.then().parser("text/html", Parser.JSON).body("responseCode", equalTo(405));
+
+        ApiUtils.attachResponse(response);
     }
-
-
 }
